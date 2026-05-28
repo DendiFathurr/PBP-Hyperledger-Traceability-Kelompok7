a4ed621f4d7791bfe346858cf265fec3126cc119
DendiFathurr <dendi.fathurrahman@gmail.com>
2026-05-28 22:51:51 +0700

    Create traceability.go

---
+++ chaincode/traceability.go
@@ -0,0 +1,7 @@
+package main
+
+// Chaincode minimal sesuai panduan [cite: 30]
+// Fungsi yang wajib ada:
+// 1. CreateAsset() - Registrasi barang
+// 2. TransferAsset() - Perubahan lokasi/logistik
+// 3. GetAssetHistory() - Audit trail dari ledger
