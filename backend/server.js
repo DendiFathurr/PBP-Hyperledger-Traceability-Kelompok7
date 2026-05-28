f4fd3c9a6ef72c027d7852aaf3b92007655b98f2
DendiFathurr <dendi.fathurrahman@gmail.com>
2026-05-28 22:54:09 +0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+// Backend API menggunakan Fabric SDK [cite: 24]
+// Bertanggung jawab menjembatani komunikasi antara Client dan Blockchain.
+const express = require('express');
+const app = express();
+// Endpoint untuk Invoke & Query akan diletakkan di sini.
